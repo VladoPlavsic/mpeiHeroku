6f36d28e0717f07fc75e0915b620b6cd0eb44021
vladoplavsic <plavsicvlado98@gmail.com>
2021-05-28 15:52:34 +0300

    Setting up yoomoney

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -59,19 +59,22 @@ router.include_router(news_delete_router, prefix="/news", tags=['news'])
 
 # test route for YOMONEEY REMOVE!!!
 
+from yookassa.domain.notification import WebhookNotification
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-# Cron job for 
-@router.get("/cron")
-def cron():
-    logger.warn("---- ACCEPTED CRON -----")
+# YooKassa Confirmation Notifications
+@router.post("users/subscriptions/notifications/", status_code=HTTP_200_OK)
+async def subscription_notification_hnd(
+    notification_object: WebhookNotification = Body(...),
+    ) -> None:
 
-@router.post("/com")
-def test(data):
-    logger.warn("------ ACCEPTED THIS -----")
-    logger.warn(data)
-    logger.warn("------ ACCEPTED THIS -----")
+    payment = notification_object.object
+    
+    logger.warn("--- ACCEPTED CONFIRMATION FROM YOOMONEY ---")
+    logger.warn(payment)
+    logger.warn("--- ACCEPTED CONFIRMATION FROM YOOMONEY ---")
+
     return None
